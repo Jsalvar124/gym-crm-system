8ba61062868acc1df0b1520530d3989eee825c29
Julián Salvá <jsalvar124@gmail.com>
2025-07-20 13:43:50 -0500

    storage init in config file

--- src/main/java/com/jsalva/gymsystem/config/AppConfig.java
+++ src/main/java/com/jsalva/gymsystem/config/AppConfig.java
@@ -1,10 +1,15 @@
 package com.jsalva.gymsystem.config;
 
+import com.jsalva.gymsystem.model.Trainer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @ComponentScan(value="com.jsalva.gymsystem")
@@ -14,5 +19,23 @@ public class AppConfig {
     public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
         return new PropertySourcesPlaceholderConfigurer();
     }
+
+    @Value("${storage.trainers.file}")
+    private String trainersFilePath;
+    @Bean
+    public Map<Long, Trainer> trainerStorage() {
+        Map<Long, Trainer> trainers = new HashMap<>();
+
+        return trainers;
+    }
+
+//    @Bean
+//    public Map<String, Map<Long, Object>> commonStorage() {
+//        Map<String, Map<Long, Object>> storage = new HashMap<>();
+//        storage.put("trainers", (Map<Long, Object>) trainerStorage());
+//        storage.put("trainees", (Map<Long, Object>) traineeStorage());
+//        storage.put("trainings", (Map<Long, Object>) trainingStorage());
+//        return storage;
+//    }
 }
 
--- src/main/java/com/jsalva/gymsystem/model/User.java
+++ src/main/java/com/jsalva/gymsystem/model/User.java
@@ -89,4 +89,5 @@ public abstract class User {
                 ", isActive=" + isActive +
                 '}';
     }
+
 }
