c30ca9b211ccf4d05c693865e88904fdb1f12386
Julián Salvá <jsalvar124@gmail.com>
2025-08-10 22:08:09 -0500

    User entity, persistence.xml file, main with EntityManagerFactory to trigger database update

--- src/main/java/com/jsalva/gymsystem/Main.java
+++ src/main/java/com/jsalva/gymsystem/Main.java
@@ -6,6 +6,8 @@ import com.jsalva.gymsystem.model.Trainee;
 import com.jsalva.gymsystem.model.Trainer;
 import com.jsalva.gymsystem.model.Training;
 import com.jsalva.gymsystem.model.TrainingType;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,60 +16,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        GymFacade gymFacade = context.getBean(GymFacade.class);
+//        GymFacade gymFacade = context.getBean(GymFacade.class);
+
 
         System.out.println("--------------------------------------------------------------------------");
-        System.out.println("GYM - CRM - SYSTEM ------------------------------------------------------");
+        System.out.println("GYM - CRM - SYSTEM - WITH DB ------------------------------------------------------");
         System.out.println("--------------------------------------------------------------------------");
 
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenceUnit");
         // Trainers
-        gymFacade.createTrainer("Juan", "Pérez", TrainingType.BOULDERING);
-        gymFacade.createTrainer("Juan", "Pérez", TrainingType.BOXING);
-
-        // Initial preloaded data goes up to id 15, 5 trainers, 10 trainees.
-        Trainer t1 = gymFacade.getTrainerById(16L);
-        Trainer t2 = gymFacade.getTrainerById(17L);
-
-        System.out.println(t1);
-        // Error, not existing id.
-        gymFacade.getTrainerById(100L);
-
-        System.out.println(t2);
-
-        gymFacade.updateTrainer(17L, "Juan", "Pulgarín", TrainingType.BOULDERING, null, null);
-
-        // Print updated trainer
-        System.out.println(t2);
-
-        gymFacade.deleteTrainer(17L);
-
-        List<Trainer> trainerList = gymFacade.getAllTrainers();
-
-        System.out.println(trainerList);
-
-        // Error, trainer deleted
-        gymFacade.getTrainerById(17L);
-
-        // Trainees
-
-        List<Trainee> traineeList = gymFacade.getAllTrainees();
-
-        gymFacade.createTrainee("Laura", "Gómez", "St 45-23", LocalDate.of(1990, 1,15));
-
-        // Trainings
-
-        List<Training> trainingList = gymFacade.getAllTrainings();
-
-        // Error, Trainer 17 no longer exists.
-        gymFacade.createTraining(17L, 18L, "Java Training", TrainingType.FUNCTIONAL, LocalDate.now(), 120);
-
-        gymFacade.createTraining(16L, 18L, "Java Training", TrainingType.FUNCTIONAL, LocalDate.now(), 120);
-
-        Training training = gymFacade.getTrainingById(21L);
-
-        System.out.println(training);
-
     }
 }
---
+++ src/main/java/com/jsalva/gymsystem/entity/User.java
@@ -0,0 +1,78 @@
+package com.jsalva.gymsystem.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public User setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public User setActive(Boolean active) {
+        isActive = active;
+        return this;
+    }
+}
